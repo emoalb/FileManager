72147dacb2abd7c76f531b6dd463f94c25e9dc7c
Emanuil Alboiadjian <32311731+emoalb@users.noreply.github.com>
2018-11-24 21:50:54 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace FileManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("File Explorer");
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.treeView1.ImageIndex = 0;
+            this.treeView1.ImageList = this.imageList1;
+            this.treeView1.Location = new System.Drawing.Point(0, 27);
+            this.treeView1.Name = "treeView1";
+            treeNode1.ImageIndex = 0;
+            treeNode1.Name = "Node0";
+            treeNode1.Text = "File Explorer";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode1});
+            this.treeView1.SelectedImageIndex = 0;
+            this.treeView1.Size = new System.Drawing.Size(581, 425);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "if_document_search_48758.png");
+            this.imageList1.Images.SetKeyName(1, "if_Open_1493293.png");
+            this.imageList1.Images.SetKeyName(2, "if_z_File_Blank_79258.png");
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(0, 0);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(581, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(581, 452);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace FileManager
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private DriveInfo[] AllDrives;
+        private Form2 form2 = new Form2();
+        private void refreshList(String directory)
+        {
+            DirectoryInfo dirinfo = new DirectoryInfo(directory);
+            treeView1.Nodes.Clear();
+            treeView1.Nodes.Add(directory);
+            if (Directory.Exists(directory))
+            {
+                try
+                {
+                    DirectoryInfo[] directories = dirinfo.GetDirectories();
+                    if (directories.Length > 0)
+                    {
+                        foreach (DirectoryInfo dir in directories)
+                        {
+
+                            TreeNode node = treeView1.Nodes[0].Nodes.Add(dir.Name);
+                            node.ImageIndex = node.SelectedImageIndex = 1;
+                            
+
+
+                        }
+                    }
+                    foreach (FileInfo file in dirinfo.GetFiles())
+                    {
+                        TreeNode node = treeView1.Nodes[0];
+                        node.ImageIndex = node.SelectedImageIndex = 1;
+                        if (file.Exists)
+                        {
+                            TreeNode nodef = treeView1.Nodes[0].Nodes.Add(file.Name);
+                            nodef.ImageIndex = nodef.SelectedImageIndex = 2;
+
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Show_Info(null, ex.Message);
+                }
+                treeView1.Nodes[0].Expand();
+
+            }
+        }
+        private void Show_Info(DriveInfo driveName, String message)
+        {
+            if (driveName!=null)
+            {
+                try
+                {
+                    form2.label1.Text = driveName.Name;
+                    form2.label2.Text = driveName.DriveType.ToString();
+                    form2.label3.Text = driveName.VolumeLabel;
+                    form2.label4.Text = driveName.DriveFormat;
+                    form2.label5.Text = driveName.AvailableFreeSpace.ToString();
+                    form2.label6.Text = driveName.TotalFreeSpace.ToString();
+                    form2.label7.Text = driveName.TotalSize.ToString();
+                }catch(Exception ex)
+                {
+                    form2.label9.Text = form2.label9.Text + "\n" + ex.Message;
+                }
+            
+            }
+            if (message != null)
+            {
+                form2.label9.Text = form2.label9.Text +"\n" +message;
+            }
+            form2.Show();
+        }
+
+        private void Load_Drive(String driveName)
+        {
+            refreshList(driveName);
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          AllDrives  = DriveInfo.GetDrives();
+            
+            int index = 0;
+            foreach (DriveInfo drive in AllDrives)
+            {
+                comboBox1.Items.Add(drive.Name);
+                index++;
+            }
+            comboBox1.SelectedIndex = 0;
+
+            Load_Drive(AllDrives[comboBox1.SelectedIndex].Name);
+            Show_Info(AllDrives[comboBox1.SelectedIndex],null);
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+         
+            Load_Drive(AllDrives[comboBox1.SelectedIndex].Name);
+            Show_Info(AllDrives[comboBox1.SelectedIndex],null);
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            form2.label9.Text = form2.label9.Text + "\n" + treeView1.SelectedNode.Text;
+       
+        }
+    }
+
+    }
+
